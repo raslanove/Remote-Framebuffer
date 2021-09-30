7a5c493033bebd3344e06678e4219bf51ba1b311
Omar El Sayyed <omar@nomone.com>
2021-09-30 11:12:33 +0200

    - Some refactoring and documentation.

--- main.c
+++ main.c
@@ -12,65 +12,94 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-Display *display;
-int screen;
-Window window;
-GC graphicsContext;
+static struct {
+    Display *display;
+    int screen;
+    Window window;
+    GC graphicsContext;
+} X11;
 
-void startX11() {
-    display = XOpenDisplay(0);
-    screen = DefaultScreen(display);
+static const char *EVENT_NAME[] = {"Reserved", "Reserved", "KeyPress", "KeyRelease", "ButtonPress", "ButtonRelease", "MotionNotify", "EnterNotify", "LeaveNotify", "FocusIn", "FocusOut", "KeymapNotify", "Expose", "GraphicsExpose", "NoExpose", "VisibilityNotify", "CreateNotify", "DestroyNotify", "UnmapNotify", "MapNotify", "MapRequest", "ReparentNotify", "ConfigureNotify", "ConfigureRequest", "GravityNotify", "ResizeRequest", "CirculateNotify", "CirculateRequest", "PropertyNotify", "SelectionClear", "SelectionRequest", "SelectionNotify", "ColormapNotify", "ClientMessage", "MappingNotify", "GenericEvent", "LASTEvent"};
+
+static void startX11() {
+
+    // Connect to X11,
+    X11.display = XOpenDisplay(0);
+    X11.screen = DefaultScreen(X11.display);
     
-    window = XCreateSimpleWindow(display, XDefaultRootWindow(display), 0, 0, 300, 300, 5, 0xffffffff, 0x00000000);
-    XSetStandardProperties(display, window, "besm Allah", "besm Allah AlRa7maan AlRa7eem", None, NULL, 0, NULL);
-    XSelectInput(display, window, ExposureMask | ButtonPressMask | KeyPressMask);
-    graphicsContext = XCreateGC(display, window, 0, 0);    
+    // Create window,
+    X11.window = XCreateSimpleWindow(
+            X11.display, XDefaultRootWindow(X11.display), 
+            0, 0, 300, 300, 
+            0 /*border width*/, XBlackPixel(X11.display, X11.screen) /*border*/,
+            0xffffffff /* background*/);
+    XSetStandardProperties(
+            X11.display, X11.window, 
+            "besm Allah" /*window name*/, "besm Allah" /*icon name*/, None /*icon*/,
+            NULL, 0, NULL); // Extra arguments.
     
-    XSetBackground(display, graphicsContext, 0xffffffff);
-    XSetForeground(display, graphicsContext, 0x00000000);
-    XClearWindow(display, window);
+    // Create graphics context,
+    X11.graphicsContext = XCreateGC(X11.display, X11.window, 0, 0);    
     
-    XMapRaised(display, window);
+    // Show the window,
+    XMapRaised(X11.display, X11.window);
+    
+    // Register input events,
+    XSelectInput(X11.display, X11.window, ExposureMask | ButtonPressMask | KeyPressMask);
 }
 
-void closeX11() {
-    XFreeGC(display, graphicsContext);
-    XDestroyWindow(display, window);
-    XCloseDisplay(display);    
+static void closeX11() {
+    XFreeGC(X11.display, X11.graphicsContext);
+    XDestroyWindow(X11.display, X11.window);
+    XCloseDisplay(X11.display);    
 }
 
-void draw() {
-    XClearWindow(display, window);
+static void draw() {
+    XSetBackground(X11.display, X11.graphicsContext, 0xffffffff);
+    XClearWindow(X11.display, X11.window);
 }
 
 int main() {
 
     XEvent event;
     KeySym key;
+    int mouseLastX=0, mouseLastY=0;
     char text[256];
 
     printf("besm Allah :)\n");
     
     startX11();
     while (1) {
-        XNextEvent(display, &event);
-        if (event.type==Expose && event.xexpose.count==0) {
-            draw();
-        }
+        
+        // Blocking function call...
+        XNextEvent(X11.display, &event);
+       
+        printf("Received event: %s\n", EVENT_NAME[event.type]);
+        
+        // Expose is received on resize (not sure if this is the only case),
+        if (event.type==Expose && event.xexpose.count==0) draw();
+        
+        // Handle key presses,
         if (event.type==KeyPress && XLookupString(&event.xkey, text, 255, &key, 0) == 1) {
             if (text[0]=='q') {
                 closeX11();
                 break;
             }
-            printf("You pressed the %c key!\n", text[0]);
         }
+        
+        // Handle mouse downs,
         if (event.type==ButtonPress) {
-            int x = event.xbutton.x, y=event.xbutton.y;
-            XSetForeground(display, graphicsContext, 0xff000000);
-            XDrawLine(display, window, graphicsContext, x, y, x, y);
-            XSetForeground(display, graphicsContext, 0xff0000ff);
+            
+            // Draw line,
+            XSetForeground(X11.display, X11.graphicsContext, 0xffff0000);
+            XDrawLine(X11.display, X11.window, X11.graphicsContext, mouseLastX, mouseLastY, event.xbutton.x, event.xbutton.y);
+            mouseLastX = event.xbutton.x;
+            mouseLastY = event.xbutton.y;
+            
+            // Draw text,
+            XSetForeground(X11.display, X11.graphicsContext, 0xff0000ff);
             strcpy(text, "Hello!");
-            XDrawString(display, window, graphicsContext, x, y, text, strlen(text));            
+            XDrawString(X11.display, X11.window, X11.graphicsContext, event.xbutton.x, event.xbutton.y, text, strlen(text));
         }
     }
     
